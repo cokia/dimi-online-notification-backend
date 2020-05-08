37b9cd3253578ca187dd5d8e12f5d35c908ac3bd
JunhoYeo <hanaro0704@gmail.com>
2020-05-09 03:47:51 +0900

    refactor: restructure methods for application initialization

--- src/app.ts
+++ src/app.ts
@@ -10,21 +10,29 @@ class App {
 
   constructor() {
     this.application = express();
+    this.initializeMiddlewares();
+    this.initializeResponseHeaders();
+    this.initializeRouter();
+  }
+
+  private initializeMiddlewares() {
+    this.application.use(cors());
+  }
+
+  private initializeResponseHeaders() {
+    this.application.all('/*', (_, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+      next();
+    });
+  }
+
+  private initializeRouter() {
+    this.application.get('/', (_, res) => {
+      res.status(200).send('dimi-online-notification api server');
+    });
   }
 }
 
 const app = new App().application;
 const zoomurl = 'zoommtg://us04web.zoom.us/join?action=join&confno=';
-
-app.use(cors());
-
-app.all('/*', (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
-  next();
-});
-
-
-app.get('/', (req, res) => {
-  res.status(200).send('dimi-online-notification api server');
-});
