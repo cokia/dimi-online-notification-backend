f6e10570c3a0e84c36a5f5199f4bba195f5543a4
cokia <hwymaster01@gmail.com>
2020-05-08 23:42:46 +0900

    fcm topic subscription add

--- src/util/fcm.ts
+++ src/util/fcm.ts
@@ -1,4 +1,6 @@
+/* eslint-disable no-throw-literal */
 import * as admin from 'firebase-admin';
+import fetch from 'node-fetch';
 import * as serviceAccount from '../../firebase-credential.json';
 
 const params = {
@@ -20,23 +22,42 @@ admin.initializeApp({
   databaseURL: 'https://dimi-online-notification.firebaseio.com/',
 });
 
-const topic = '';
-const payload:any = {
-  notification: {
-    title,
-    body,
-    click_action,
-    icon,
-  },
-  ,
-  topic,
-};
-
-
-admin.messaging().send(payload)
-  .then((response) => {
-    console.log('Successfully sent message:', response);
-  })
-  .catch((error) => {
-    console.log('Error sending message:', error);
+export async function subscribeTokenToTopic(token, topic) {
+  fetch(`https://iid.googleapis.com/iid/v1/${token}/rel/topics/${topic}`, {
+    method: 'POST',
+    headers: new Headers({
+      Authorization: `key=${fcm_server_key}`,
+    }),
+  }).then((response) => {
+    if (response.status < 200 || response.status >= 400) {
+      throw `Error subscribing to topic: ${response.status} - ${response.text()}`;
+    }
+    console.log(`Subscribed to "${topic}"`);
+  }).catch((error) => {
+    console.error(error);
   });
+}
+export async function fcmsend(title:string, body:string, url:string, topic:string) {
+  const payload:any = {
+    notification: {
+      title,
+      body,
+      click_action: url,
+      icon: 'https://github.com/cokia.png',
+    },
+    topic,
+  };
+
+
+  admin.messaging().send(payload)
+    .then((response) => {
+      console.log('Successfully sent message:', response);
+    })
+    .catch((error) => {
+      console.log('Error sending message:', error);
+    });
+}
+
+export async function test() {
+  console.log('test');
+}
